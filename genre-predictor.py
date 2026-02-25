eb0108bb678fb0391c83cffa068041b02702477a
bbhall <bbhall@ualberta.ca>
2026-02-25 11:49:49 -0700

    fully working temp solution

--- genre-predictor.py
+++ genre-predictor.py
@@ -1,5 +1,5 @@
 from python_speech_features import mfcc
-import scipy.io.wavefile as wav
+import scipy.io.wavfile as wav
 import numpy as np
 import math
 from tempfile import TemporaryFile
@@ -8,6 +8,18 @@ import pickle
 import random
 import operator
 
+def distance(instance1, instance2, k):
+    distance = 0
+    mm1 = instance1[0]
+    cm1 = instance1[1]
+    mm2 = instance2[0]
+    cm2 = instance2[1]
+    distance = np.trace(np.dot(np.linalg.inv(cm2), cm1))
+    distance+=(np.dot(np.dot((mm2-mm1).transpose() , np.linalg.inv(cm2)) , mm2-mm1 )) 
+    distance+= np.log(np.linalg.det(cm2)) - np.log(np.linalg.det(cm1))
+    distance-= k
+    return distance
+
 """
 Get distance between feature vectors and find neighbours
 """
@@ -15,8 +27,8 @@ def getNeighbours(trainingSet, instance, k):
     distances = []
     for i in range(len(trainingSet)):
         dist = distance(trainingSet[i], instance, k) + distance(instance, trainingSet[i], k)
-        distance.append((trainingSet[i][2], dist))
-    distance.sort(key=operator.itemgetter(1))
+        distances.append((trainingSet[i][2], dist))
+    distances.sort(key=operator.itemgetter(1))
     neighbours = []
     for i in range(k):
         neighbours.append(distances[i][0])
@@ -35,7 +47,7 @@ def nearestClass(neighbours):
         else:
             classVote[response] = 1
     
-    sorter = sorted(classVote.items(), key = operator.itemgetter[1], reverse=True)
+    sorter = sorted(classVote.items(), key = operator.itemgetter(1), reverse=True)
     return sorter[0][0]
 
 """
@@ -48,20 +60,24 @@ def getAccuracy(testSet, predictions):
             correct += 1
     return 1.0*correct/len(testSet)
 
-data_path = "data" # path to data set
+data_path = "data/genres_original/" # path to data set
 f = open("my.dat", 'wb')
 
+i = 0
 for folder in os.listdir(data_path):
     i += 1
     if i == 11:
         break
     for file in os.listdir(data_path+folder):
-        (rate,sig) = wav.read(data_path+folder+"/"+file)
-        mfcc_feat = mfcc(sig, rate, winlen=0.020, appendEnergy=False)
-        covariance = np.cov(np.matrix.transpose(mfcc_feat))
-        mean_matrix = mfcc_feat.mean(0)
-        feature = (mean_matrix, covariance, i)
-        pickle.dump(feature, f)
+        try:
+            (rate,sig) = wav.read(data_path+folder+"/"+file)
+            mfcc_feat = mfcc(sig, rate, winlen=0.020, appendEnergy=False)
+            covariance = np.cov(np.matrix.transpose(mfcc_feat))
+            mean_matrix = mfcc_feat.mean(0)
+            feature = (mean_matrix, covariance, i)
+            pickle.dump(feature, f)
+        except ValueError:
+            print(f"Could not read {file}, skipping...")
 
 f.close()
 
