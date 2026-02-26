a4ff69018bcbe4e4190102c1252401dbfc811a51
bbhall <bbhall@ualberta.ca>
2026-02-25 21:03:00 -0700

    adding comments to functions

--- genre-predictor.py
+++ genre-predictor.py
@@ -8,6 +8,16 @@ import pickle
 import random
 import operator
 
+"""
+
+
+Parameters:
+    instance1   -
+    instance2   -
+    k           -
+Returns:
+    distance    -
+"""
 def distance(instance1, instance2, k):
     distance = 0
     mm1 = instance1[0]
@@ -22,6 +32,13 @@ def distance(instance1, instance2, k):
 
 """
 Get distance between feature vectors and find neighbours
+
+Paramters:
+    trainingSet - data set of training inputs
+    instance    - 
+    k           - 
+Returns:
+    neighbours  - 
 """
 def getNeighbours(trainingSet, instance, k):
     distances = []
@@ -36,6 +53,11 @@ def getNeighbours(trainingSet, instance, k):
 
 """
 Identify the nearest neighbours
+
+Parameters:
+    neighbours  - 
+Returns:
+    sorter      - 
 """
 def nearestClass(neighbours):
     classVote = {}
@@ -52,6 +74,12 @@ def nearestClass(neighbours):
 
 """
 Model evaluation
+
+Parameters:
+    testSet     - array of randomly selected testing data
+    predictions - array of predictions for each test input
+Returns:
+    accuracy    - % of test values that were correctly classified by the model
 """
 def getAccuracy(testSet, predictions):
     correct = 0
@@ -60,30 +88,20 @@ def getAccuracy(testSet, predictions):
             correct += 1
     return 1.0*correct/len(testSet)
 
-data_path = "data/genres_original/" # path to data set
-f = open("my.dat", 'wb')
+"""
+Loads dataset from file and randomly splits it into training and testing datasets
 
-i = 0
-for folder in os.listdir(data_path):
-    i += 1
-    if i == 11:
-        break
-    for file in os.listdir(data_path+folder):
-        try:
-            (rate,sig) = wav.read(data_path+folder+"/"+file)
-            mfcc_feat = mfcc(sig, rate, winlen=0.020, appendEnergy=False)
-            covariance = np.cov(np.matrix.transpose(mfcc_feat))
-            mean_matrix = mfcc_feat.mean(0)
-            feature = (mean_matrix, covariance, i)
-            pickle.dump(feature, f)
-        except ValueError:
-            print(f"Could not read {file}, skipping...")
-
-f.close()
-
-dataset = []
-def loadDataset(filename, split, trainSet, testSet):
-    with open("my.dat", 'rb') as f:
+Parameters:
+    filename    - name of file that stores the properites of each music file
+    splits      - value between 0-1 that tells how to split the data between testing and training sets
+    dataset     - array of all audio files
+    trainSet    - array of randomly selected training audio files
+    testSet     - array of randomly selected testing audio files
+Returns:
+    N/A
+"""
+def loadDataset(filename, split, dataset, trainSet, testSet):
+    with open(filename, 'rb') as f:
         while True:
             try:
                 dataset.append(pickle.load(f))
@@ -97,13 +115,46 @@ def loadDataset(filename, split, trainSet, testSet):
         else:
             testSet.append(dataset[i])
 
-trainingSet = []
-testingSet = []
-loadDataset("my.dat", 0.66, trainingSet, testingSet)
 
-predictions = []
-for i in range(len(testingSet)):
-    predictions.append(nearestClass(getNeighbours(trainingSet, testingSet[i], 5)))
 
-accuracy1 = getAccuracy(testingSet, predictions)
-print(accuracy1)
+def main():
+    data_path = "data/genres_original/" # path to data set
+    f = open("my.dat", 'wb')    # open file to store dataset information
+
+    i = 0
+    for folder in os.listdir(data_path):
+        i += 1
+        if i == 11: # Only 10 genres in training set
+            break
+        for file in os.listdir(data_path+folder):   # Loop through each file in a genre folder
+            try:
+                # Get audio file properties and store information in my.dat file
+                (rate,sig) = wav.read(data_path+folder+"/"+file)
+                mfcc_feat = mfcc(sig, rate, winlen=0.020, appendEnergy=False)
+                covariance = np.cov(np.matrix.transpose(mfcc_feat))
+                mean_matrix = mfcc_feat.mean(0)
+                feature = (mean_matrix, covariance, i)
+                pickle.dump(feature, f)
+            except ValueError:
+                # Skip invalid files
+                print(f"Could not read {file}, skipping...")
+
+    f.close()   # Close my.dat file
+
+    # Create data set partitions and load the data
+    dataset = []
+    trainingSet = []
+    testingSet = []
+    loadDataset("my.dat", 0.66, dataset, trainingSet, testingSet)
+
+    # Get predictions for testing data using k-nearest neighbours
+    predictions = []
+    for i in range(len(testingSet)):
+        predictions.append(nearestClass(getNeighbours(trainingSet, testingSet[i], 5)))
+
+    # Calculate accuracy of model
+    accuracy1 = getAccuracy(testingSet, predictions)
+    print(accuracy1)
+
+if __name__ == "__main__":
+    main()
