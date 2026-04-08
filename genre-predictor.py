7c380ae08330b2ad9299c0d69eb5a318c8a29b08
Benjamin Salmon <bsalmon@ulaberta.ca>
2026-04-07 19:55:50 -0600

    Genre : song

--- genre-predictor.py
+++ genre-predictor.py
@@ -178,19 +178,19 @@ def datasetSplit():
     genres = list(sorted(os.listdir("data/genres_original/")))
     print("\n=============================================")
     for i in range(len(testDataset)):
-        print(testDataset[i][3],end=": ")
+        
         genreIdx = nearestClass(getNeighbours(trainDataset, testDataset[i], k))
         if (testDataset[i][2] != -1):
             # Only add predictions for custom files that have been sorted and add to sorted testing set
             sortedTestingSet.append(testDataset[i])
             predictions.append(genreIdx)
-        print(genres[genreIdx-1])
+        print(f"{genres[genreIdx-1]:<15}: {testDataset[i][3]}")
     print("=============================================")
     accuracy = getAccuracy(sortedTestingSet, predictions)
     print(f"ACCURACY: {accuracy}")
 
 
-"""
+""":
 Creates new dat file from default audio data
 
 Parameters:
